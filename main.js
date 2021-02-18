00cdefa2ae513dc75ccc1fbc15b789ded67f5a14
“Willie” <willie.williams@codeimmersives.com>
2021-02-18 13:04:44 -0500

    finished

--- main.js
+++ main.js
@@ -2,11 +2,37 @@
  * YOUR CODE HERE! *
  *******************/
 
+ function yell(string) {
+   return string + "!";
+ }
 
+function getFirstCharacter(string) {
+  return string[0];
+}
 
+function getLastCharacter(string) {
+  return string[string.length - 1];
+}
 
+function getOneCharacter(string, number) {
+  return string[number];
+}
+function getTwoCharacters(string, number, number1) {
+  return string[number] + string[number1];
+}
 
+function makeCapitalized(string) {
+  return string.toUpperCase();
+}
 
+function yellLouder(string) {
+  return string.toUpperCase() + '!!!';
+}
+
+function getInitials(h){
+  const i = h.indexOf(' ');
+  return h[0] + '.' + h[i+1] + '.';
+}
 /********************************************************************
  * THIS CODE IS FOR INTERNAL USE ONLY. DON'T CHANGE ANYTHING BELOW! *
  ********************************************************************/
